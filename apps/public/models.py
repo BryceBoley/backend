c963e142c30fd30b63f9c5f7a5c4ebbe42abaf24
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-04 23:56:57 -0700

    Created UserProfile model, also updated Admin.py

--- apps/public/models.py
+++ apps/public/models.py
@@ -2,19 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
+
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True, help_text="Describe your group")
     location = models.TextField(help_text="What can people put into maps to get to your house")
     # members = models.ManyToManyField(User)
-    alergies = models.TextField(blank=True, null=True)
+    food_allergies = models.TextField(blank=True, null=True)
     pics = models.ImageField(upload_to='photos', blank=True, null=True)
     # creator = models.OneToOneField(User)
     # date = models.parse_datetime()
 
     def __str__(self):
         return self.name
+
+
+class UserProfile(models.Model):
+    # user = models.ForeignKey(User)
+    phone_number = models.CharField(max_length=15)
+    street = models.CharField(max_length=75)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=35)
+    food_allergies = models.TextField(max_length=75)
+    profile_picture = models.ImageField(upload_to='photos', blank=True, null=True)
+
+    def __str__(self):
+        return u"%s" % self.user  # Need to change to user (according to model)
+
 
 # class Group(models.Model):
 #     name = models.CharField(max_length=100)
