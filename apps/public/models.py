adb88eca9e35f25ea23cd4ca6aef0c2ff6cd03b8
Bryce Boley <bryceboley1@gmail.com>
2015-02-09 14:16:48 -0700

    got all from friday working again

--- apps/public/models.py
+++ apps/public/models.py
@@ -4,22 +4,20 @@ from django.contrib.auth.models import Group
 
 
 class Event(models.Model):
-	# host = models.ForeignKey(User)
 	host = models.CharField(max_length=50)
-	description = models.TextField(blank=True, help_text="Describe your group")
 	location = models.TextField(help_text="Where should members go for your dinner group?")
-	# attendees = models.ForeignKey(Groups)
-	food_allergies = models.TextField(blank=True)
-	pics = models.ImageField(upload_to='photos', blank=True, null=True)
-	# creator = models.OneToOneField(User)
-	# date = models.parse_datetime()
+	when = models.TextField(blank=True, help_text="Where event is to take place")
+	description = models.TextField(blank=True, help_text="Describe your group")
+	members = models.TextField(blank=True)
+	comments = models.TextField(blank=True)
+	#pics = models.ImageField(upload_to='photos', blank=True, null=True)
 
 	def __str__(self):
 		return self.host
 
 
 class Member(models.Model):
-	name = models.CharField(max_length=250)
+	name = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name
