90d51eb34e62da3d2ffe95eb84abd3a01d279a2a
Bryce Boley <bryceboley1@gmail.com>
2015-02-11 13:56:14 -0700

    changed models back to way they should be

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns(
     url(r'^add_event$', AddEvent.as_view(), name='add_event'),
     url(r'^members/$', MemberList.as_view(), name='members_list'),
     url(r'^add_member$', AddMember.as_view(), name='add_member'),
-    url(r'^events/(?P<pk>[0-9]+)$', EditEvent.as_view(), name='event_list'),
-    url(r'^members/(?P<pk>[0-9]+)$', EditMember.as_view(), name='members_list'),
+    url(r'^edit_event/(?P<pk>[0-9]+)$', EditEvent.as_view(), name='event_list'),
+    url(r'^edit_member/(?P<pk>[0-9]+)$', EditMember.as_view(), name='members_list'),
     # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 )
