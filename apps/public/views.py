a4622dc3752a2cb52baa249689595cfc045d28f3
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-16 13:05:50 -0700

    pep 8 tidy up

--- apps/public/views.py
+++ apps/public/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from serializers import *
 
+
 class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
