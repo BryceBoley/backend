84daa29c20367ed4caef287bb0050d0f61e27a3f
Bryce Boley <bryceboley1@gmail.com>
2015-02-28 12:16:13 -0700

    clean up models, remove unused imports

--- apps/public/models.py
+++ apps/public/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
 from datetime import datetime
-
-# Create your models here.
 
 
 class Event(models.Model):
